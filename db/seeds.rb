4259a24ed404c7b3277049956ce619fcf9f7b465
Lara Klemme <laraklemme@gmail.com>
2021-08-11 16:00:40 +0200

    refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+mcdonalds = { name: 'McDonalds', address: 'Checkpoint Charlie', phone_number: '0123 4567654', category: 'french' }
+burgerking = { name: 'Burger King', address: 'Brandenburger Tor', phone_number: '0123 5909385', category: 'belgian' }
+sushi = { name: 'Makate', address: 'Tiergarten', phone_number: '0123 459024', category: 'japanese' }
+pizza = { name: 'Marios', address: 'Alexander Platz', phone_number: '0123 4858050', category: 'italian' }
+noodles = { name: 'Flying Dragon', address: 'Isenstedt', phone_number: '0123 59383', category: 'chinese' }
+
+[mcdonalds, burgerking, sushi, pizza, noodles].each do |hash|
+  restaurant = Restaurant.create!(hash)
+  puts "Created #{restaurant.name}"
+end
